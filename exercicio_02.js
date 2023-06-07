587091b6cc33d37929e4a14e46548a918beb0a2c
luispneu <luisgabriel8328@gmail.com>
2023-06-07 16:57:44 -0400

    exercicio 2 meu error

--- exercicio_02.js
+++ exercicio_02.js
@@ -1,44 +1,43 @@
-/*
-  Todos os atributos necessários. Assim, usando Try e Catch, trate os erros e apresente uma mensagem quando houve atributos vazios.
-*/
+class MeuErro extends Error {
+  constructor(message){
+    super(message);
+    this.name = "Meu Erro";
+  }
+}
 
-class Estudante {
-  constructor(nome, idade, turma) {
+class Animal {
+  constructor(nome, idade, especie) {
     this.nome = nome;
     this.idade = idade;
-    this.turma = turma;
-    
-    if(!nome || !idade || !turma){
-      if (this.nome != "" && this.idade != "" && this.especie != ""){
-      } else{ 
-      throw  new Error("todos os atributos tem que estar completos")
-    }
-    }
+    this.especie = especie;
   }
 
   mostrarAtributos(){
-    try{
-      return this.atributos();
-    } catch(erro){
-      console.log(erro.stack)
+    try {
+      return this.atributos();  
+    } catch (error) {
+      console.log(error) 
     }
   } 
 
   atributos() {
-    return {
-      nome: this.nome,
-      idade: this.idade,
-      turma: this.turma
-    };
+    if (this.nome != ""){
+      return {
+        nome: this.nome,
+        idade: this.idade,
+        especie: this.especie
+      };
+    } else {
+      throw new MeuErro("Deu erro")
+    }
   }
 }
 
-const estudante = new Estudante("", 16, "3A");
+const meuAnimal = new Animal("", 3, "cachorro");
+const atributos = meuAnimal.mostrarAtributos();
 
-const atributos = estudante.mostrarAtributos();
-
-console.log(atributos.nome);   
-console.log(atributos.idade);  
-
-console.log(atributos.turma); 
-
+console.log(atributos)
+// Imprimindo os atributos
+// console.log(atributos.nome);   
+// console.log(atributos.idade);  
+// console.log(atributos.especie); 
